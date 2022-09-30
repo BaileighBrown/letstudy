1461f41064301caf7c398099f3816e0e08fa2e17
Baileigh <baileighbrown@Baileighs-MacBook-Pro.local>
2022-09-30 15:22:37 -0400

    login functionality created

--- backend/controllers/auth/postLogin.js
+++ backend/controllers/auth/postLogin.js
@@ -1,5 +1,34 @@
+const User = require('../../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+
 const postLogin = async (req, res) => {
-    res.send('login route');
+   try {
+const { mail, password } = req.body;
+
+const user = await User.findOne({ mail: mail.toLowerCase() }); 
+
+//if the user is found 
+if(user && (await bcrypt.compare(password, user.password) )){
+    // send new token 
+    const token = "JWT_TOKEN";
+
+    return res.status(200).json({ 
+        userDetails: { 
+            mail:user.mail, 
+            token:token,
+            username:user.username,
+        },
+    });
+}
+
+//if user is not found
+return res.status(400).send('invalid credentials. try again ')
+
+   } catch (err) {
+    return res.status(500).send('Something went wrong. Please try again');
+   }
 };
 
 module.exports = postLogin;

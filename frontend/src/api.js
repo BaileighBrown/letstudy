a1a51904e6c3d3f42f8020d1adb409abb299cdd2
Baileigh <baileighbrown@Baileighs-MacBook-Pro.local>
2022-10-01 18:31:57 -0400

    jwt token requests, and fixing returns

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,26 @@
 // allows use to communicate with api 
-
 // axios makes sending asynchronous HTTP requests to REST endpoints easier and helps you perform CRUD operations.
 import axios from 'axios';
+import { logout } from './shared/utils/auth';
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:5002/api',
     timeout: 1000,
 });
 
+apiClient.interceptors.request.use((config)=>{
+    const userDetails = localStorage.getItem('user');
+//if we get any tokens from he user details 
+    if(userDetails){
+    const token = JSON.parse(userDetails).token;
+    config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (err) => {
+    return Promise.reject(err);
+})
+
+//public routes, whats available on the outside 
 // responsible for making a request to the server. here we are tring to connect the server side with the client side
 export const login = async (data) => {
     try {
@@ -31,3 +44,12 @@ export const register = async (data) => {
         };
     }
 };
+
+//secure routes for only the returned tokens are correct
+const checkResponseCode = (exception) => {
+    const responseCode = exception?.response?.status;
+
+    if (responseCode){
+        (responseCode === 401 || responseCode === 403) && logout();
+    }
+}

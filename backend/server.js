82501deeaf0e181c7a196aecf004f522ff76a654
Baileigh <baileighbrown@Baileighs-MacBook-Pro.local>
2022-10-11 16:04:51 -0400

    tweaked server json

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const server = http.createServer(app);
 socketServer.registerSocketServer(server)
 
 //connecting to mongo atlas database, if connects successfully it will console listen on port 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     //starting the server
 server.listen(PORT, () =>{

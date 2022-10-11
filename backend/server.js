c8758d84fc0db9d6c492d7116fa992f3d0255737
Baileigh <baileighbrown@Baileighs-MacBook-Pro.local>
2022-10-10 22:18:35 -0400

    changed routing again

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static(path.join(__dirname, '../frontend/build')));
 }
-app.get('*',(req, res) => {
+app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 ///////////////////////

b7851a7f1dfede630d6b0464049641506b17db15
Baileigh <baileighbrown@Baileighs-MacBook-Pro.local>
2022-10-11 00:19:16 -0400

    added parser

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const server = http.createServer(app);
 socketServer.registerSocketServer(server)
 
 //connecting to mongo atlas database, if connects successfully it will console listen on port 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 .then(() => {
     //starting the server
 server.listen(PORT, () =>{

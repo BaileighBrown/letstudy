a31f55a9e0e63e73cd568a6d79e1325c1f869725
Baileigh <baileighbrown@Baileighs-MacBook-Pro.local>
2022-10-12 20:23:35 -0400

    reverted back to inital connection

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const server = http.createServer(app);
 socketServer.registerSocketServer(server)
 
 //connecting to mongo atlas database, if connects successfully it will console listen on port 
-/*mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     //starting the server
 server.listen(PORT, () =>{
@@ -52,8 +52,4 @@ server.listen(PORT, () =>{
 .catch((err) => {
     console.log("database connection failed. Server not started");
     console.error(err);
-  });*/
-const db = process.env.MONGO_URI
-  mongoose.connect(db)
-  .then(()=> console.log("mongodb has connected"))
-  .catch((err)=> console.log(err))
+  });
